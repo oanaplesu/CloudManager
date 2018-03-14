8e7597222cbc09d8700ceaf2d34ebd39f2dfc44b
Oana Plesu <plesu.oana@gmail.com>
2018-03-14 22:52:11 +0200

    initial setup to use google drive api

--- app/src/main/java/com/oanaplesu/cloudmanager/MainActivity.java
+++ app/src/main/java/com/oanaplesu/cloudmanager/MainActivity.java
@@ -1,13 +1,54 @@
 package com.oanaplesu.cloudmanager;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.Scope;
+import com.google.api.services.drive.DriveScopes;
 
 public class MainActivity extends AppCompatActivity {
+    final private int REQUEST_CODE_SIGN_IN = 1;
+
+    private GoogleSignInClient mGoogleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        signIn();
+    }
+
+    private void signIn() {
+        mGoogleSignInClient = buildGoogleSignInClient();
+        mGoogleSignInClient.signOut();
+        startActivityForResult(mGoogleSignInClient.getSignInIntent(), REQUEST_CODE_SIGN_IN);
+    }
+
+    private GoogleSignInClient buildGoogleSignInClient() {
+        GoogleSignInOptions signInOptions =
+                new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                        .requestScopes(new Scope(DriveScopes.DRIVE))
+                        .build();
+        return GoogleSignIn.getClient(this, signInOptions);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_CODE_SIGN_IN) {
+            if (resultCode == RESULT_OK) {
+                Toast.makeText(this, "Sign in successful", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Sign in failed", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
